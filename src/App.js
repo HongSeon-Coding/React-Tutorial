3b58ea3d8d3cffbe67be277fe183063dbc38bf11
= <ahs9702@daum.net>
2020-07-25 23:27:26 +0900

    First Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Hello from './Hello';
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color:'aqua',
+    fontSize:24,
+    padding:'1rem'
+  };
+    //JSX 규칙
+    //1. tag는 반드시 닫아야한다. input,br 등 조심
+    //2. 2개 이상의 tag는 하나의 tag로 감싸야한다.
+    //2.1 <></> 도 사용해도 된다.
+    {/*주석주석주석*/}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Hello/>
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+// 컴포넌트 생성하기 대문자로 시작할 것!
+function Hello(){
+    
+    return (
+        <div>안녕하세요!!</div>
+    )
+}
+
+
+// Hello 컴포넌트를 모듈화 하기!!
+export default Hello;
